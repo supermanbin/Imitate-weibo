004f69e03c84fbde09533ed492383dd42953b837
fengbinsuperman <fengbinsuperman@gmail.com>
2016-08-12 18:02:55 +0800

    normal commit code
    normal commit code

---
+++ FBweibo/FBweibo/Class/CodeGuideLine.m
@@ -0,0 +1,32 @@
+//
+//  CodeGuideLine.m
+//  FBweibo
+//
+//  Created by 封斌 on 16/8/12.
+//  Copyright © 2016年 supermanUI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#pragma mark - Lifecycle
+//- (instancetype)init {}
+//- (void)dealloc {}
+//- (void)viewDidLoad {}
+//- (void)viewWillAppear:(BOOL)animated {}
+//- (void)didReceiveMemoryWarning {}
+#pragma mark - Custom Accessors
+//- (void)setCustomProperty:(id)value {}
+//- (id)customProperty {}
+#pragma mark - IBActions
+//- (IBAction)submitData:(id)sender {}
+#pragma mark - Public
+//- (void)publicMethod {}
+#pragma mark - Private
+//- (void)privateMethod {}
+#pragma mark - Protocol conformance
+#pragma mark - UITextFieldDelegate
+#pragma mark - UITableViewDataSource
+#pragma mark - UITableViewDelegate
+#pragma mark - NSCopying
+//- (id)copyWithZone:(NSZone *)zone {}
+#pragma mark - NSObject
+//- (NSString *)description {}
--- FBweibo/FBweibo/Class/Home/Controller/HomeViewController.m
+++ FBweibo/FBweibo/Class/Home/Controller/HomeViewController.m
@@ -7,14 +7,16 @@
 //
 
 #import "HomeViewController.h"
-#import "ButtonOfHomeTitle.h"
 #import "CustomView.h"
+#import <pop/POP.h>
 
 @interface HomeViewController ()
-
+@property (nonatomic, weak) CustomView *customV;
 @end
 
 @implementation HomeViewController
+
+#pragma Mark - Lifecycle
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -30,24 +32,28 @@
                    forState:UIControlStateNormal];
     [titleBtn setImage:[UIImage imageNamed:@"navigationbar_arrow_down"]
               forState:UIControlStateNormal];
-    [titleBtn setImage:[UIImage imageNamed:@"navigationbar_arrow_up"]
-              forState:UIControlStateSelected];
 
+    [titleBtn addTarget:self action:@selector(buttonTouchDownAction:)
+       forControlEvents:UIControlEventTouchDown | UIControlEventTouchDragEnter];
+    [titleBtn addTarget:self action:@selector(buttonTouchUpInsideAction:)
+       forControlEvents:UIControlEventTouchUpInside];
     
-//    titleBtn.backgroundColor = [UIColor groupTableViewBackgroundColor];
-//    titleBtn.imageView.backgroundColor = [UIColor lightGrayColor];
-//    titleBtn.titleLabel.backgroundColor = [UIColor lightTextColor];
-//    titleBtn.titleLabel.font = [UIFont boldSystemFontOfSize:14];
-//    titleBtn.titleLabel.textAlignment = NSTextAlignmentLeft;
-//    titleBtn.titleLabel.lineBreakMode = NSLineBreakByTruncatingTail;
+    titleBtn.adjustsImageWhenHighlighted = NO; // 去除点击是图片上的阴影
+    titleBtn.backgroundColor = [UIColor whiteColor];
+    titleBtn.layer.cornerRadius = 3.0;
+    titleBtn.contentEdgeInsets = UIEdgeInsetsMake(0.0, 6.0, 0.0, 6.0);
+    titleBtn.titleLabel.font = [UIFont boldSystemFontOfSize:14];
+    titleBtn.titleLabel.textAlignment = NSTextAlignmentLeft;
+    titleBtn.titleLabel.lineBreakMode = NSLineBreakByTruncatingTail;
     
     self.navigationItem.titleView = titleBtn;
     
-    CustomView *customV = [[CustomView alloc] init];
-    customV.frame = CGRectMake(100, 100, 100, 100);
-    [self.view addSubview:customV];
 }
 
+#pragma Mark - Custom Accessores
+
+
+#pragma Mark - Private Method
 - (void)friendatTenttionAction {
     FBLog(@"friendatTenttionAction !!!!");
 }
@@ -55,5 +61,32 @@
 - (void)radarAction {
     FBLog(@"radarAction !!!!");
 }
+
+#pragma Mark - Button Action
+/**
+ *  navigationItem titleView touchDown action handle
+ */
+- (void)buttonTouchDownAction:(UIButton *)sender {
+    POPBasicAnimation *backgroundAnimation = [POPBasicAnimation animationWithPropertyNamed:kPOPLayerBackgroundColor];
+    backgroundAnimation.toValue = [UIColor groupTableViewBackgroundColor];
+    backgroundAnimation.duration = 0.3;
+    [sender.layer pop_addAnimation:backgroundAnimation forKey:@"buttonLayerHighlightColor"];
+}
+
+/**
+ *  navigationItem titleView touchUpInside action handle
+ */
+- (void)buttonTouchUpInsideAction:(UIButton *)sender {
+    POPBasicAnimation *backgroundAnimation = [POPBasicAnimation animationWithPropertyNamed:kPOPLayerBackgroundColor];
+    backgroundAnimation.toValue = [UIColor whiteColor];
+    [sender.layer pop_addAnimation:backgroundAnimation forKey:@"buttonLayerNormalColor"];
+    
+    POPBasicAnimation *rotationAnimation = [POPBasicAnimation animationWithPropertyNamed:kPOPLayerRotationX];
+    rotationAnimation.toValue = @(M_PI);
+    rotationAnimation.duration = 0.3;
+    [sender.imageView.layer pop_addAnimation:rotationAnimation forKey:@"buttonImageViewRotation"];
+}
+
+
 
 @end
